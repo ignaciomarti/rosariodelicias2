30f68a267510482f1cab6be24499a5fc339afa42
Ignacio <mfignacio97@hotmail.com>
2020-08-11 22:38:01 -0300

    bug fixes

--- resources/views/front/productos/producto.blade.php
+++ resources/views/front/productos/producto.blade.php
@@ -43,7 +43,9 @@
                         <h2 class="mb-4 producto-titulo p-2 main-color text-center">Productos similares</h2>
                         <div class="list-group lista-producto">
                             @forelse ($producto->categoria->productos as $productos)
+                            @if ($productos != $producto)
                             <a href="{{route('productos.show', $productos->id)}}" class="list-group-item">{{$productos->titulo}}</a>
+                            @endif
                             @empty
                             No hay productos similares
                             @endforelse
